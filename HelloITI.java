84a9a4e1d5d1e0dc52f8cbccc4b28d0fc7025b2a
kim <rawan.mohamed21k@gmail.com>
2024-07-21 16:42:31 +0300

    added print hellogit class

--- HelloITI.java
+++ HelloITI.java
@@ -1,5 +1,10 @@
-public class HelloWorld {
+public class HelloJava {
+	static void printHelloGit(){
+	system.out.println("Hello, Git");	
+	}
+	
     public static void main(String[] args) {
         System.out.println("Hello, ITI!");
+		HelloJava.printHelloGit();
     }
 }
