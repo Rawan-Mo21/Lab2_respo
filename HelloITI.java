850466dde6c2841c3bf343ce84fae03d531ca539
AbanobN <abanob.nabil.nagib@gmail.com>
2024-07-22 15:56:46 +0300

    Make Branch And Test it

--- HelloITI.java
+++ HelloITI.java
@@ -2,6 +2,9 @@ public class HelloJava {
 	static void printHelloGit(){
 	system.out.println("Hello, Git");	
 	}
+	static void printHelloBranch(){
+	system.out.println("Hello, Branch");	
+	}
 	
     public static void main(String[] args) {
         System.out.println("Hello, ITI!");
